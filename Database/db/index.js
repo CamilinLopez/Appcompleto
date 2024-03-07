c5546ad85113b6374a5aa9a1d42762bbf13f7207
CamilinLopez <camilollc98@gmail.com>
2024-03-06 22:08:33 -0500

    rename db folder to src

--- Database/db/index.js
+++
@@ -1,4 +0,0 @@
-require("dotenv").config();
-const { sequelize } = require("sequelize");
-const { EXTERNAL_DATABASE_URL } = process.env;
-
---
+++ Database/src/models/index.js
@@ -0,0 +1,4 @@
+export const Carritos = require("./Carrito");
+export const Productos = require("./Productos");
+export const ProductosCarrito = require("./ProductosCarrito");
+export const Users = require("./Users");
